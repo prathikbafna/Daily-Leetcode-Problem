cb817346efda17fc34f399febfd7d3d94cffb8cc
prathikbafna <44114369+prathikbafna@users.noreply.github.com>
2025-10-08 20:04:07 -0600

    Oct 8th Daily LC challenge

---
+++ LC_10_08_2025.py
@@ -0,0 +1,31 @@
+"""
+
+3494. Find the Minimum Amount of Time to Brew Potions
+
+Medium
+
+You are given two integer arrays, skill and mana, of length n and m, respectively.
+
+In a laboratory, n wizards must brew m potions in order. Each potion has a mana capacity mana[j] and must pass through all the wizards sequentially to be brewed properly. The time taken by the ith wizard on the jth potion is timeij = skill[i] * mana[j].
+
+Since the brewing process is delicate, a potion must be passed to the next wizard immediately after the current wizard completes their work. This means the timing must be synchronized so that each wizard begins working on a potion exactly when it arrives. ​
+
+Return the minimum amount of time required for the potions to be brewed properly.
+
+
+"""
+
+
+
+class Solution:
+    def minTime(self, skill: List[int], mana: List[int]) -> int:
+        n, m = len(skill), len(mana)
+        done = [0] * (n + 1)
+        
+        for j in range(m):
+            for i in range(n):
+                done[i + 1] = max(done[i + 1], done[i]) + mana[j] * skill[i]
+            for i in range(n - 1, 0, -1):
+                done[i] = done[i + 1] - mana[j] * skill[i]
+                
+        return done[n]
